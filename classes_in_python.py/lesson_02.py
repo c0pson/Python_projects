7a663cb890a63fc5c1cef8bf7dc076cece74b4b3
Piotr Copek <pc312199@student.polsl.pl>
2024-02-15 12:00:18 +0100

    lesson 2

--- classes_in_python.py/lesson_02.py
+++ classes_in_python.py/lesson_02.py
@@ -1,11 +1,13 @@
 class Character:
 
     level = 1
+    characters_on_map = 0
 
     def __init__(self, health, damage, name):
         self.health = health
         self.damage = damage
         self.name = name
+        Character.characters_on_map += 1
 
     def print_stats(self):
         print(f'{self.name} stats: Health: {self.health} | Damage: {self.damage} | Level: {self.level}')
@@ -13,13 +15,17 @@ class Character:
     def level_up(self):
         self.level += 1
 
-yeti = Character(health=200, damage=15, name='Yeti')
-lizard = Character(health=100, damage=40, name='Lizard')
+print(Character.characters_on_map)
 
-# yeti.print_stats()
-# lizard.print_stats()
+yeti = Character(health=200, damage=15, name='Yeti')
+print(yeti.__dict__)
+print(Character.characters_on_map)
+
+lizard = Character(health=100, damage=40, name='Lizard')
+print(Character.characters_on_map)
 
 yeti.level_up()
+print(yeti.__dict__)
 yeti.level_up()
 lizard.level_up()
 
