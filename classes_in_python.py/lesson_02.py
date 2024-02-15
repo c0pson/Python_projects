d3e0a176d2f62d3eefb6acda5c617e62f1a1174b
Piotr Copek <pc312199@student.polsl.pl>
2024-02-15 11:57:27 +0100

    lesson 2

--- classes_in_python.py/lesson_02.py
+++ classes_in_python.py/lesson_02.py
@@ -1,18 +1,27 @@
 class Character:
 
+    level = 1
+
     def __init__(self, health, damage, name):
         self.health = health
         self.damage = damage
         self.name = name
 
     def print_stats(self):
-        print(f'{self.name} stats: Health: {self.health} | Damage: {self.damage}')
+        print(f'{self.name} stats: Health: {self.health} | Damage: {self.damage} | Level: {self.level}')
+
+    def level_up(self):
+        self.level += 1
 
 yeti = Character(health=200, damage=15, name='Yeti')
 lizard = Character(health=100, damage=40, name='Lizard')
 
-print(yeti)
-print(lizard)
+# yeti.print_stats()
+# lizard.print_stats()
+
+yeti.level_up()
+yeti.level_up()
+lizard.level_up()
 
 yeti.print_stats()
 lizard.print_stats()
