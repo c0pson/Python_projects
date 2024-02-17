36e05cbf64b22e9f45ed2db15aac545608133288
Piotr Copek <pc312199@student.polsl.pl>
2024-02-17 08:48:14 +0100

    handling two players and ball is moving

--- pong/pong_game.py
+++ pong/pong_game.py
@@ -23,16 +23,49 @@ class Player:
         self.player = pygame.draw.rect(screen, self.color, self.player_rect)
 
     def update(self, direction):
+        if self.pos_y == HEIGHT - self.height and direction == 1:
+            direction = 0
+        if self.pos_y == 0 and direction == -1:
+            direction = 0
         self.pos_y = self.pos_y + self.speed * direction
         self.player_rect = (self.pos_x, self.pos_y, self.width, self.height)
 
 class Ball:
-    ...
+    def __init__(self, pos_x, pos_y, size, speed, color):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.size = size
+        self.speed = speed
+        self.color = color
+
+        self.move_x = 1
+        self.move_y = 1
+
+    def display(self):
+        if self.pos_y == HEIGHT - self.size:
+            self.move_y = -1
+        if self.pos_y == 0 + self.size:
+            self.move_y = 1
+        if self.pos_x == WIDTH - self.size:
+            self.move_x = -1
+        if self.pos_x == 0 + self.size:
+            self.move_x = 1 # there will be points system
+
+    def hit(self):
+        ...
+
+        self.ball = pygame.draw.circle(screen, self.color, (self.pos_x, self.pos_y), self.size)
+
+    def move(self):
+        self.pos_x = self.pos_x + self.move_x * self.speed
+        self.pos_y = self.pos_y + self.move_y * self.speed
+
 
 def main():
     running = True
-    player_1 = Player(20, HEIGHT//2-100, 20, 100, 10, 'green')
-    player_2 = Player(WIDTH - 40, HEIGHT//2-100, 20, 100, 10, 'red')
+    player_1 = Player(20, HEIGHT // 2 - 100, 20, 100, 10, 'green')
+    player_2 = Player(WIDTH - 40, HEIGHT // 2 - 100, 20, 100, 10, 'red')
+    ball = Ball(WIDTH//2-20, HEIGHT//2-20, 20, 10, 'blue')
     player_1_direction = 0
     player_2_direction = 0
     while running:
@@ -49,7 +82,7 @@ def main():
                     player_1_direction = -1
                 if event.key == pygame.K_s:
                     player_1_direction = 1
-
+    # not intuitive - to change
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     player_2_direction = 0
@@ -58,10 +91,14 @@ def main():
 
         player_1.update(player_1_direction)
         player_2.update(player_2_direction)
+        ball.move()
 
         player_1.display()
         player_2.display()
+        ball.display()
 
         clock.tick(FPS)
         pygame.display.update()
-main()
+
+if __name__ == "__main__":
+    main()
