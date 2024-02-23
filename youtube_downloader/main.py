eaf691c8cfd4a209355a9e9c12108de4de83fb85
Piotr Copek <pc312199@student.polsl.pl>
2024-02-23 20:25:23 +0100

    basics to start

--- youtube_downloader/main.py
+++ youtube_downloader/main.py
@@ -1,14 +1,23 @@
 from pytube import YouTube
 
-url = 'https://youtu.be/zFD20EQHpUM'
+url: str = 'https://youtu.be/zFD20EQHpUM'
 
 yt = YouTube(url)
 streams = yt.streams.all()
 
-all_res = []
+all_res: list = []
 
 for s in streams:
     all_res.append(s.resolution)
     print(s.resolution)
 
 print(all_res)
+
+resolutions: list = list(set(all_res))
+resolutions: list = [x for x in resolutions if x is not None]
+resolutions: list = [res.replace('p', '') for res in resolutions]
+resolutions: list = [int(res) for  res in resolutions]
+resolutions.sort()
+
+print(resolutions)
+
