f283bd34f37fd42bb1a09431e1a5cfc6aab179ca
Piotr Copek <pc312199@student.polsl.pl>
2024-02-25 19:22:58 +0100

    almost ready

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -11,11 +11,28 @@ def press_tab(cell):
 def handle_delete():
     pyautogui.hotkey('shift', 'tab')
 
-def enter_click_handle(guess_list, word, attempt_nb, entry_boxes_to_color):
+def remove_all(app):
+    widgets = app.winfo_children()
+    for widget in widgets:
+        widget.destroy()
+
+def win(app, my_font, guess_list, attempt_nb, word, entry_boxes):
+    remove_all(app)
+    pop_up = ctk.CTkFrame(app)
+    pop_up.pack(expand=True)
+
+    win_label = ctk.CTkLabel(master=pop_up, width=140, height=40, font=my_font, text='YOU WON!!')
+    win_label.pack(side='top', expand=True, padx=10, pady=10)
+
+    reset_button = ctk.CTkButton(master=pop_up, width=140, height=40, font=my_font, text='PLAY AGAIN', command=lambda: restart_app(app, my_font, guess_list, attempt_nb, word, entry_boxes))
+    reset_button.pack(side='top', expand=True, padx=10, pady=10)
+
+def enter_click_handle(app, guess_list, word, attempt_nb, entry_boxes_to_color, my_font, entry_boxes):
     word_from_list = ''.join(guess_list)
     list_of_words = load_all_words(load_directory())
+    counter = 0
     if '' in guess_list:
-        print('Ayo wtf')
+        pass
     if word_from_list in list_of_words and '' not in guess_list:
         attempt_nb[0] += 1
         row_index = attempt_nb[0] - 2
@@ -24,11 +41,15 @@ def enter_click_handle(guess_list, word, attempt_nb, entry_boxes_to_color):
             index_in_row = row_index * 5 + i
             if guess_list[i] == word[i]:
                 entry_boxes_to_color[index_in_row].configure(fg_color='green')
+                counter += 1
             elif guess_list[i] in word and guess_list[i] not in highlighted_letters:
                 entry_boxes_to_color[index_in_row].configure(fg_color='blue')
                 highlighted_letters.add(guess_list[i])
         guess_list.clear()
         guess_list.extend([''] * 5)
+    if counter == 5:
+        win(app, my_font, guess_list, attempt_nb, word, entry_boxes)
+    counter = 0
 
 def check_row(event, entry_box, row_nb, attempt_nb, cell):
     check_length(event, entry_box, cell)
@@ -55,7 +76,9 @@ def check_length(event, entry_box, cell):
         entry_box.delete('0', 'end')
     if event.keysym == 'Return':
         pyautogui.press('Tab')
-    if event.keysym == 'BackSpace' and cell != 1:
+    if event.keysym == 'BackSpace' and cell != 1 and cell != 5:
+        handle_delete()
+    if event.keysym == 'BackSpace' and entry_box.get() == '' and cell == 5:
         handle_delete()
     entry_box.configure(state='normal')
 
@@ -83,7 +106,7 @@ def get_random_word(words):
     return word_as_list
 
 def entry_frame_handle(app, my_font, cell, guess_list, row_nb, attempt_nb):
-    entry = ctk.CTkEntry(master=app, font=my_font, width=60, height=60)
+    entry = ctk.CTkEntry(master=app, font=my_font, width=44, height=42)
     entry.configure(insertontime=0)
     entry.grid(row=row_nb-1, column=cell-1, padx=5, pady=5, sticky='nsew')
     entry.bind(sequence='<KeyPress>', command=lambda event: check_row(event, entry, row_nb, attempt_nb, cell))
@@ -120,17 +143,24 @@ def button_frame(app, my_font, guess_list, word, attempt_nb, entry_boxes):
     frame_for_buttons = ctk.CTkFrame(master=app, width=360, corner_radius=10)
     frame_for_buttons.pack(side='top', fill='both', expand=True, padx=5, pady=5)
 
-    enter_button = ctk.CTkButton(master=frame_for_buttons, text='Enter', font=my_font, width=100, height=72, command=lambda: enter_click_handle(guess_list, word, attempt_nb, entry_boxes))
+    enter_button = ctk.CTkButton(master=frame_for_buttons, text='Enter', font=my_font, width=100, height=72, command=lambda: enter_click_handle(app, guess_list, word, attempt_nb, entry_boxes, my_font, entry_boxes))
     enter_button.pack(expand=True)
+
+def restart_app(app, my_font, guess_list, attempt_nb, word, entry_boxes):
+    guess_list.clear()
+    guess_list.extend([''] * 5)
+    attempt_nb[0] = 1
+    word = get_random_word(load_directory())
+    remove_all(app)
+    main_frame(app, my_font, guess_list, attempt_nb, word)
 
 def main():
     attempt_nb = [1]
     words = load_directory()
     word = get_random_word(words)
     guess_list = [''] * 5
-    print(word)
 
-    WIDTH, HEIGHT = 420, 680
+    WIDTH, HEIGHT = 520, 680
     app = ctk.CTk()
     app.geometry(f'{WIDTH}x{HEIGHT}')
     app.title('WORDLE')
