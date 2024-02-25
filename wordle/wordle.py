ad225f5b50b8217a8429644d9cc04ecba8a71b2b
Piotr Copek <pc312199@student.polsl.pl>
2024-02-25 17:49:11 +0100

    its getting harder and harder

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -3,19 +3,34 @@ import pyautogui
 import random
 import os
 
-def enter_click_handle(guess_list, word, attempt_nb):
-    print(guess_list)
-    for i in range(5):
-        if guess_list[i] == word[i]:
-            print(f'Letter {i+1} is correct')
+def press_tab(cell):
+    if cell == 5:
+        return
+    pyautogui.press('tab')
 
-    attempt_nb[0] = int(attempt_nb[0]) + 1
-    print(attempt_nb)
-    return attempt_nb
+def handle_delete(cell):
+    pyautogui.hotkey('shift', 'tab')
 
-#main logic
+def enter_click_handle(guess_list, word, attempt_nb, entry_boxes_to_color):
+    word_from_list = ''.join(guess_list)
+    list_of_words = load_all_words(load_directory())
+    if '' in guess_list:
+        print('Ayo wtf')
+    if word_from_list in list_of_words and '' not in guess_list:
+        attempt_nb[0] += 1
+        row_index = attempt_nb[0] - 2
+        for i in range(5):
+            index_in_row = row_index * 5 + i
+            if guess_list[i] == word[i]:
+                entry_boxes_to_color[index_in_row].configure(fg_color='green')
+            elif guess_list[i] in word:
+                entry_boxes_to_color[index_in_row].configure(fg_color='blue')
+        guess_list.clear()
+        guess_list.extend([''] * 5)
 
-def check_row(event, entry_box, row_nb, attempt_nb):
+def check_row(event, entry_box, row_nb, attempt_nb, cell):
+    if event.keysym == 'Delete':
+        print('try it nerd')
     check_length(event, entry_box)
     if row_nb != attempt_nb[0]:
         return 'break'
@@ -32,7 +47,7 @@ def get_letter(event, cell, guess_list, entry_box):
     entry_box.delete('0', 'end')
     entry_box.insert(0, entry_text)
     entry_box.update()
-    print(guess_list)
+    press_tab(cell)
 
 def check_length(event, entry_box):
     if len(entry_box.get()) > 0 and event.keysym != 'Tab' and event.keysym != 'Return': # 'Return' is somehow 'Enter'
@@ -45,14 +60,19 @@ def load_directory():
     word_list = os.path.join(working_directory, 'words.txt')
     return word_list
 
-def get_random_word(words):
+def load_all_words(words):
     list_of_words = []
-    word_as_list = []
     with open(words, 'r') as words_list:
         for word in words_list:
             list_of_words.append(word.strip('\n'))
-        word = random.choice(list_of_words)
         words_list.close()
+    return list_of_words
+
+def get_random_word(words):
+    list_of_words = []
+    word_as_list = []
+    list_of_words = load_all_words(words)
+    word = random.choice(list_of_words)
     for letter in word:
         word_as_list.append(letter)
     print(word_as_list)
@@ -62,40 +82,41 @@ def entry_frame_handle(app, my_font, cell, guess_list, row_nb, attempt_nb):
     entry = ctk.CTkEntry(master=app, font=my_font, width=60, height=60)
     entry.configure(insertontime=0)
     entry.grid(row=row_nb-1, column=cell-1, padx=5, pady=5, sticky='nsew')
-    entry.bind(sequence='<KeyPress>', command=lambda event: check_row(event, entry, row_nb, attempt_nb))
+    entry.bind(sequence='<KeyPress>', command=lambda event: check_row(event, entry, row_nb, attempt_nb, cell))
     entry.bind(sequence='<KeyRelease>', command=lambda event: get_letter(event, cell, guess_list, entry))
+    return entry
 
 def setup_line(frame, my_font, guess_list, row, attempt_nb):
-    entry_frame_handle(frame, my_font, 1, guess_list, row, attempt_nb)
-    entry_frame_handle(frame, my_font, 2, guess_list, row, attempt_nb)
-    entry_frame_handle(frame, my_font, 3, guess_list, row, attempt_nb)
-    entry_frame_handle(frame, my_font, 4, guess_list, row, attempt_nb)
-    entry_frame_handle(frame, my_font, 5, guess_list, row, attempt_nb)
+    entries = [] # makes it possible to then color the right cell
+    for cell in range(1, 6):
+        entry = entry_frame_handle(frame, my_font, cell, guess_list, row, attempt_nb)
+        entries.append(entry)
+    return entries
 
-def main_frame(app, my_font, guess_list, attempt_nb):
+def main_frame(app, my_font, guess_list, attempt_nb, word):
     frame_for_words = ctk.CTkFrame(master=app, corner_radius=10)
     frame_for_words.pack(side='top', fill='both', expand=True, padx=5, pady=5)
 
     centring_frame = ctk.CTkFrame(master=frame_for_words, corner_radius=10, fg_color='transparent')
     centring_frame.pack(side='top', expand=True, padx=5, pady=5)
 
-    setup_line(centring_frame, my_font, guess_list, 1, attempt_nb)
-    setup_line(centring_frame, my_font, guess_list, 2, attempt_nb)
-    setup_line(centring_frame, my_font, guess_list, 3, attempt_nb)
-    setup_line(centring_frame, my_font, guess_list, 4, attempt_nb)
-    setup_line(centring_frame, my_font, guess_list, 5, attempt_nb)
-    setup_line(centring_frame, my_font, guess_list, 6, attempt_nb)
+    entry_boxes = [] # makes it possible to then color the right cell
+    for row in range(1, 7):
+        row_entry_boxes = setup_line(centring_frame, my_font, guess_list, row, attempt_nb)
+        entry_boxes.extend(row_entry_boxes)
 
     frame_for_words.grid_rowconfigure(0, weight=1)
     frame_for_words.grid_rowconfigure(7, weight=1)
     frame_for_words.grid_columnconfigure(0, weight=1)
     frame_for_words.grid_columnconfigure(6, weight=1)
 
-def button_frame(app, my_font, guess_list, word, attempt_nb):
+    button_frame(app, my_font, guess_list, word, attempt_nb, entry_boxes)
+
+def button_frame(app, my_font, guess_list, word, attempt_nb, entry_boxes):
     frame_for_buttons = ctk.CTkFrame(master=app, width=360, corner_radius=10)
     frame_for_buttons.pack(side='top', fill='both', expand=True, padx=5, pady=5)
 
-    enter_button = ctk.CTkButton(master=frame_for_buttons, text='Enter', font=my_font, width=100, height=72, command=lambda: enter_click_handle(guess_list, word, attempt_nb))
+    enter_button = ctk.CTkButton(master=frame_for_buttons, text='Enter', font=my_font, width=100, height=72, command=lambda: enter_click_handle(guess_list, word, attempt_nb, entry_boxes))
     enter_button.pack(expand=True)
 
 def main():
@@ -111,9 +132,7 @@ def main():
     app.title('WORDLE')
     my_font = ctk.CTkFont(family='Hack Nerd Font Regular', size=44)
 
-    main_frame(app, my_font, guess_list, attempt_nb)
-
-    button_frame(app, my_font, guess_list, word, attempt_nb)
+    main_frame(app, my_font, guess_list, attempt_nb, word)
 
     app.mainloop()
 
