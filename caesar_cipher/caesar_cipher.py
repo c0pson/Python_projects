e84eaacbe586af344c50b25dd1c6d8da4c5ffd01
Piotr Copek <pc312199@student.polsl.pl>
2024-02-27 13:40:59 +0100

    added reading from file

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,40 +1,38 @@
-def get_mode_from_user(alphabet):
+import os
+
+def get_mode_from_user() -> int:
     while True:
-        user_input = input('Encryption\\Decryption in python\n[1 -> encrypt]\n[2 -> decrypt from key]\n[3 -> brute force decrypt]\n:')
+        user_inp = input('Encryption/Decryption in python using Caesar cipher\n[1 -> encrypt]\n[2 -> decrypt from key]\n[3 -> brute force decrypt]\n:')
         try:
-            user_input = int(user_input)
-            if user_input == 1:
-                mode_1(alphabet)
-                return
-            elif user_input == 2:
-                mode_2(alphabet)
-                return
-            elif user_input == 3:
-                mode_3(alphabet)
-                return
-            else:
-                print('Provide only number associated with modes provided above')
+            user_inp = int(user_inp)
+            if user_inp == 1:
+                return user_inp
+            if user_inp == 2:
+                return user_inp
+            if user_inp == 3:
+                return user_inp
+            print('Provide only number associated with modes provided above')
         except ValueError:
             print('Provide only number associated with modes provided above')
 
-def mode_1(alphabet):
+def mode_1(alphabet) -> None:
     shift = get_shift_from_user()
     shifted_alphabet = key(alphabet, shift)
     text = get_text_from_user()
     encoded_text = encode(text, shifted_alphabet, alphabet)
-    print(''.join(encoded_text))
+    save_text(''.join(encoded_text))
 
-def mode_2(alphabet):
+def mode_2(alphabet) -> None:
     shift = get_shift_from_user()
     text = get_text_from_user()
     shifted_alphabet = key(alphabet, shift)
     decoded_text = decode_from_key(text, shifted_alphabet, alphabet)
-    print(''.join(decoded_text))
+    save_text(''.join(decoded_text))
 
-def mode_3(alphabet):
+def mode_3(alphabet) -> None:
     text = get_text_from_user()
     brute_force_texts = decode_brute_force(text, alphabet)
-    print(''.join(brute_force_texts))
+    save_text(''.join(brute_force_texts))
 
 def get_shift_from_user() -> int:
     alphabet_len = 26
@@ -50,8 +48,19 @@ def get_shift_from_user() -> int:
             print('Provide integer number!')
 
 def get_text_from_user() -> str:
-    user_input = input('Enter text: ')
+    path_to_text = os.path.dirname(__file__)
+    path_to_text = os.path.join(path_to_text, 'text.txt')
+    with open(path_to_text, 'r') as file:
+        user_input = file.read()
+    file.close()
     return user_input
+
+def save_text(text) -> None:
+    path_to_file = os.path.dirname(__file__)
+    path_to_file = os.path.join(path_to_file, 'text_after_man.txt')
+    with open(path_to_file, 'w') as file:
+        file.write(text)
+    file.close()
 
 def key(alphabet: list[str], shift: int) -> list[str]:
     shifted_alphabet = [''] * len(alphabet)
@@ -62,9 +71,8 @@ def key(alphabet: list[str], shift: int) -> list[str]:
 def encode(text: str, key: list[str], alphabet: list[str]) -> list[str]:
     encoded_text = [''] * len(text)
     counter = 0
-    special_characters = ' .,?!;:-!@#$%^&*()=+_\\[]{};\'\"<>?/~`1234567890'
     for letter in text:
-        if letter.lower() in special_characters:
+        if letter.lower() not in alphabet:
             encoded_text[counter] = letter
         else:
             if letter.islower():
@@ -77,9 +85,8 @@ def encode(text: str, key: list[str], alphabet: list[str]) -> list[str]:
 def decode_from_key(text: str, key: list[str], alphabet: list[str]) -> list[str]:
     decoded_text = [''] * len(text)
     counter = 0
-    special_characters = ' .,?!;:-!@#$%^&*()=+_\\[]{};\'\"<>?/~`1234567890'
     for letter in text:
-        if letter.lower() in special_characters:
+        if letter.lower() not in alphabet:
             decoded_text[counter] = letter
         else:
             if letter.islower():
@@ -92,12 +99,11 @@ def decode_from_key(text: str, key: list[str], alphabet: list[str]) -> list[str]
 def decode_brute_force(text: str, alphabet: list[str]) -> list[str]:
     decoded_text = [''] * len(text)
     brute_force_tries = [''] * len(alphabet)
-    special_characters = ' .,?!;:-!@#$%^&*()=+_\\[]{};\'\"<>?/~`1234567890'
     for i in range(len(alphabet)):
         counter = 0
         shifted_alphabet = key(alphabet, i)
         for letter in text:
-            if letter.lower() in special_characters:
+            if letter.lower() not in alphabet:
                 decoded_text[counter] = letter
             else:
                 if letter.islower():
@@ -105,14 +111,20 @@ def decode_brute_force(text: str, alphabet: list[str]) -> list[str]:
                 else:
                     decoded_text[counter] = alphabet[shifted_alphabet.index(letter.lower())].upper()
             counter += 1
-        brute_force_tries[i] = ''.join(decoded_text)
+        brute_force_tries[i] = f'Attempt {i+1}: \n\n' + ''.join(decoded_text) + '\n\n'
         decoded_text = [''] * len(text)
     return brute_force_tries
 
 def main() -> None:
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    get_mode_from_user(alphabet)
+    mode = get_mode_from_user()
+    if mode == 1:
+        mode_1(alphabet)
+    elif mode == 2:
+        mode_2(alphabet)
+    elif mode == 3:
+        mode_3(alphabet)
 
 if __name__ == "__main__":
     main()
