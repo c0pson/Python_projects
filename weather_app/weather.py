f87ab6e1c5d3ebc232f94ce831194ad29515475d
Piotr Copek <pc312199@student.polsl.pl>
2024-03-16 11:44:40 +0100

    working call and visuals

--- weather_app/weather.py
+++ weather_app/weather.py
@@ -5,7 +5,7 @@ import requests
 import time
 import os
 
-def get_request():
+def print_weather(icon, city, weather, temp, feel_temp, humidity, rain_vol):
     function_map = {
         '01d': ascii_images._01d,
         '02d': ascii_images._02d,
@@ -26,8 +26,36 @@ def get_request():
         '13n': ascii_images._13n,
         '50n': ascii_images._50n,
     }
-    zip_code = '41-707'
+    ascii_art = function_map[icon]()
+    lines = ascii_art.split('\n')
+    current_time = time.time()
+    current_datetime = datetime.fromtimestamp(current_time)
+    time_and_location = f'''
+\t\t╭─────────────────Current─weather─────────────────╮
+\t\t│╭───────────────────────────────────────────────╮│
+\t\t││ City: {city}{' '*(40-len(str(city)))}││
+\t\t││ Time: {current_datetime.strftime('%H:%M:%S')}{' '*32}││
+\t\t│╰───────────────────────────────────────────────╯│'''
+    lines[1] +=  '╭──────────────────────────────╮│'
+    lines[2] += f'│ {weather}{' '*(29-len(str(weather)))}││' # type: ignore
+    lines[3] += f'│ Temperature: {temp}°C{' '*(14-len(str(temp)))}││' # type: ignore
+    lines[4] += f'│ Feel temperature: {feel_temp}°C{' '*(9-len(str(feel_temp)))}││' # type: ignore
+    lines[5] += f'│ Humidity: {humidity}%{' '*(18-len(str(humidity)))}││' # type: ignore
+    lines[6] += f'│ {rain_vol}{' '*(29-len(str(rain_vol)))}││' # type: ignore
+    lines[7] +=  '╰──────────────────────────────╯│'
+
+    os.system('cls')
+    print(time_and_location, end='')
+    for i, item in enumerate(lines):
+        if i != len(lines)-1:
+            print('\t\t'+item.strip())
+        else:
+            print('\t\t'+item.strip(), end='')
+    print('╰─────────────────────────────────────────────────╯\n')
+
+def get_request():
     country = 'PL'
+    zip_code = '41-707'
     city_name = requests.get(f'http://api.openweathermap.org/geo/1.0/zip?zip={zip_code},{country}&appid={API}')
     if city_name.status_code == 200:
         data = city_name.json()
@@ -50,58 +78,12 @@ def get_request():
             rain_vol = data['rain']['1h']
         else:
             rain_vol = ''
-
-        ascii_art = function_map[icon]()
-        lines = ascii_art.split('\n')
-
-        current_time = time.time()
-        current_datetime = datetime.fromtimestamp(current_time)
-
-        time_and_location = f'''
-\t\t╭───────────────────────────────────────────────╮
-\t\t│ {city}{' '*(46-len(str(city)))}│
-\t\t│ {current_datetime.strftime('%H:%M:%S')}{' '*38}│
-\t\t╰───────────────────────────────────────────────╯'''
-
-        lines[1] +=  '╭──────────────────────────────╮'
-        lines[2] += f'│ {weather}{' '*(29-len(str(weather)))}│' # type: ignore
-        lines[3] += f'│ Temperature: {temp}°C{' '*(14-len(str(temp)))}│' # type: ignore
-        lines[4] += f'│ Feel temperature: {feel_temp}°C{' '*(9-len(str(feel_temp)))}│' # type: ignore
-        lines[5] += f'│ Hymidity: {humidity}%{' '*(18-len(str(humidity)))}│' # type: ignore
-        lines[6] += f'│ {rain_vol}{' '*(29-len(str(rain_vol)))}│' # type: ignore
-        lines[7] +=  '╰──────────────────────────────╯'
-
-        os.system('cls')
-        print(time_and_location, end='')
-        for item in lines:
-            print('\t\t'+item.strip())
-
+        print_weather(icon, city, weather, temp, feel_temp, humidity, rain_vol)
     elif response.status_code == 400:
         print('NOT FOUND!')
 
 def main():
     get_request()
-    # print('DAY: ')
-    # ascii_images._01d()
-    # ascii_images._02d()
-    # ascii_images._03d()
-    # ascii_images._04d()
-    # ascii_images._09d()
-    # ascii_images._10d()
-    # ascii_images._11d()
-    # ascii_images._13d()
-    # ascii_images._50d()
-
-    # print('NIGHT: ')
-    # ascii_images._01n()
-    # ascii_images._02n()
-    # ascii_images._03n()
-    # ascii_images._04n()
-    # ascii_images._09n()
-    # ascii_images._10n()
-    # ascii_images._11n()
-    # ascii_images._13n()
-    # ascii_images._50n()
 
 if __name__ == "__main__":
     main()
