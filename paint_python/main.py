73b64e8220c2012623ead24e30a9ea75a8a8d112
Piotr Copek <pc312199@student.polsl.pl>
2024-03-21 23:59:25 +0100

    test

--- paint_python/main.py
+++ paint_python/main.py
@@ -1,23 +1,23 @@
-import tkinter as tk
+import customtkinter as ctk
 
 def reset(event, old_x, old_y):
     old_x[0] = None
     old_y[0] = None
 
 def print_dot(event, canvas):
-    canvas.create_oval(event.x-1, event.y-1, event.x+1, event.y+1, width=2, fill='black')
+    canvas.create_oval(event.x-1, event.y-1, event.x+1, event.y+1, width=3, fill='black')
 
 def paint_line(event, canvas, old_x, old_y):
     if old_x[0] and old_y[0]:
-        canvas.create_line(old_x[0], old_y[0], event.x, event.y, width=3, fill='black', capstyle=tk.ROUND, smooth=tk.TRUE, splinesteps=1)
+        canvas.create_line(old_x[0], old_y[0], event.x, event.y, width=6, fill='black', capstyle=ctk.ROUND, smooth=ctk.TRUE, splinesteps=1)
     old_x[0] = event.x
     old_y[0] = event.y
 
 def main():
-    app = tk.Tk()
+    app = ctk.CTk()
     old_x = [None]
     old_y = [None]
-    canvas = tk.Canvas(app)
+    canvas = ctk.CTkCanvas(app)
     canvas.pack(fill='both', expand=True)
 
     app.bind('<B1-Motion>', lambda event: paint_line(event, canvas, old_x, old_y))
