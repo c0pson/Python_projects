d0c31bf13671467c9c20f82aa53f44bb6addfacd
Piotr Copek <pc312199@student.polsl.pl>
2024-04-05 21:40:11 +0200

    working keyboard handling and search for repeating numbers

--- sudoku/main.py
+++ sudoku/main.py
@@ -28,6 +28,7 @@ class Board(ctk.CTkFrame):
             pass
         elif event.keysym in accepted_input:
             self.selected_cell.configure(text=f'{event.keysym}')
+        self.move_on_board(event)
 
     def cell_clicked(self, cell):
         if not self.selected_cell:
@@ -56,6 +57,66 @@ class Board(ctk.CTkFrame):
             for j in range(3):
                 self.create_cells_box(i, j%3)
 
+    def find_index(self, list, value):
+        for i in range(len(list)):
+            for j in range(len(list[0])):
+                if list[i][j] == value:
+                    return(i, j)
+        return (0, 0)
+
+    def move_on_board(self, event):
+        board_ids = [
+            [0,  1,  2,  9,  10, 11, 18, 19, 20],
+            [3,  4,  5,  12, 13, 14, 21, 22, 23],
+            [6,  7,  8,  15, 16, 17, 24, 25, 26],
+            [27, 28, 29, 36, 37, 38, 45, 46, 47],
+            [30, 31, 32, 39, 40, 41, 48, 49, 50],
+            [33, 34, 35, 42, 43, 44, 51, 52, 53],
+            [54, 55, 56, 63, 64, 65, 72, 73, 74],
+            [57, 58, 59, 66, 67, 68, 75, 76, 77],
+            [60, 61, 62, 69, 70, 71, 78, 79, 80]
+        ]
+        if self.selected_cell is None:
+            return
+        if event.keysym == 'Up' or event.keysym == 'w':
+            current_index = self.all_cells.index(self.selected_cell)
+            x, y = self.find_index(board_ids, current_index)
+            if not x:
+                return
+            if self.selected_cell:
+                self.selected_cell.configure(fg_color=Colors.TEXT)
+                self.all_cells[board_ids[x-1][y]].configure(fg_color=Colors.BUT1)
+                self.selected_cell = self.all_cells[board_ids[x-1][y]]
+        if event.keysym == 'Down' or event.keysym == 's':
+            current_index = self.all_cells.index(self.selected_cell)
+            x, y = self.find_index(board_ids, current_index)
+            if x > 7:
+                return
+            if self.selected_cell:
+                self.selected_cell.configure(fg_color=Colors.TEXT)
+                self.all_cells[board_ids[x+1][y]].configure(fg_color=Colors.BUT1)
+                self.selected_cell = self.all_cells[board_ids[x+1][y]]
+        if event.keysym == 'Left' or event.keysym == 'a':
+            current_index = self.all_cells.index(self.selected_cell)
+            x, y = self.find_index(board_ids, current_index)
+            if not y:
+                return
+            if self.selected_cell:
+                self.selected_cell.configure(fg_color=Colors.TEXT)
+                self.all_cells[board_ids[x][y-1]].configure(fg_color=Colors.BUT1)
+                self.selected_cell = self.all_cells[board_ids[x][y-1]]
+        if event.keysym == 'Right' or event.keysym == 'd':
+            current_index = self.all_cells.index(self.selected_cell)
+            x, y = self.find_index(board_ids, current_index)
+            if y > 7:
+                return
+            if self.selected_cell:
+                self.selected_cell.configure(fg_color=Colors.TEXT)
+                self.all_cells[board_ids[x][y+1]].configure(fg_color=Colors.BUT1)
+                self.selected_cell = self.all_cells[board_ids[x][y+1]]
+
+    # TODO: detect which is incorrect | in next version - now just basics
+    # alghoritm was made for fun | kinda unpractical but was fun to make
     def check_board(self):
         # check in box
         cells = []
@@ -77,7 +138,7 @@ class Board(ctk.CTkFrame):
             if i%3 == 0 and i != 0:
                 multi += 2
             for j in range(9):
-                index = j + (6*(j//3)) + (i*3) + (3*3*multi)
+                index = j + (6*(j//3)) + (i*3) + (9*multi)
                 if self.all_cells[index].cget('text') != '   ':
                     rows.append(self.all_cells[index].cget('text'))
             if len(rows) != len(set(rows)):
@@ -91,7 +152,7 @@ class Board(ctk.CTkFrame):
             if i%3 == 0 and i != 0:
                 multi += 2
             for j in range(9):
-                index = 3*j + 4*4*(j//3) + (j//3)*2 + i + (3*multi)
+                index = 3*j + 16*(j//3) + (j//3)*2 + i + (3*multi)
                 if self.all_cells[index].cget('text') != '   ':
                     columns.append(self.all_cells[index].cget('text'))
             if len(columns) != len(set(columns)):
@@ -101,22 +162,6 @@ class Board(ctk.CTkFrame):
             columns = []
         if 0 in is_good:
             print('u good bro?')
-
-"""
-Board tiles ids
-
-0  1  2  |  9 10 11 | 18 19 20
-3  4  5  | 12 13 14 | 21 22 23
-6  7  8  | 15 16 17 | 24 25 26
-------------------------------
-27 28 29 | 36 37 38 | 45 46 47
-30 31 32 | 39 40 41 | 48 49 50
-33 34 35 | 42 43 44 | 51 52 53
-------------------------------
-54 55 56 | 63 64 65 | 72 73 74
-57 58 59 | 66 67 68 | 75 76 77
-60 61 62 | 69 70 71 | 78 79 80
-"""
 
 class App(ctk.CTk):
     def __init__(self):
